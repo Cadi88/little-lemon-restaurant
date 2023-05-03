930d969a13138c40090ac887a3c1879166d47a3d
Daniel Enriquez <daniel.enriquez@talkoot.com>
2023-05-03 14:32:42 -0600

    initial setup

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <img />
+      <div>
+        Doormat Navigation Home
+        About
+        menu
+        Reservations
+        Order Online
+        Login
+      </div>
+      <div>
+        Contact
+        Adress
+        phone number
+        email
+
+      </div>
+      <div>
+        Social Media Links
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
